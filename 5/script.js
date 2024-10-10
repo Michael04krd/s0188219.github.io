386d3af65fd93101fb7d252a285cf585a4b61625
Michael04krd <bob.michael0405@mail.ru>
2024-10-10 23:46:58 +0300

    Update script.js

--- 5/script.js
+++ 5/script.js
@@ -1,16 +1,15 @@
 document.getElementById("ug").addEventListener("click", function () {
     const kolvo = document.getElementById("kol").value;
     const good = document.getElementById("good").value;
+    const resul = document.getElementById("result");
     const res = kolvo * good;
-    
 
     const reg = /^[1-9][0-9]*$/;
-    if(reg.test(kolvo)){
-        document.getElementById("result").textContent = "Стоимость товара: " + res;
-        document.getElementById("result").style.cssText = "opacity: 1";
-    }
-    else {
-        document.getElementById("result").textContent = "Некорректное количество товара!";
-        document.getElementById("result").style.cssText = "opacity: 1; background-color: red";
+    if (reg.test(kolvo)) {
+        resul.textContent = "Стоимость товара: " + res;
+        resul.style.cssText = "opacity: 1";
+    } else {
+        resul.textContent = "Некорректное количество товара!";
+        resul.style.cssText = "opacity: 1; background-color: red";
     }
 });
